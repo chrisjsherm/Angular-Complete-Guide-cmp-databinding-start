9e723fc0468e700653ee8235da1b855e48b3640f
Chris Sherman <chris.sherman@outlook.com>
2019-02-23 10:31:00 -0500

    Add communication between AppComponent and CockpitComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,19 @@ export class AppComponent {
     },
   ];
 
+  onAddServer(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onAddBlueprint(blueprintData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent,
+    });
+  }
 }
