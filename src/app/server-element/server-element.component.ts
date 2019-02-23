66c51a8a436325c4bc485fc420a344cc6b04bd2b
Chris Sherman <chris.sherman@outlook.com>
2019-02-23 12:10:44 -0500

    Lifecycle demo.

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, AfterViewInit {
   @Input() element: { type: string, name: string, content: string };
-
-  constructor() { }
+  @ViewChild('heading') heading: ElementRef;
+  constructor() {
+  }
 
   ngOnInit() {
+    console.log(`Local reference heading ngOnInit: ${this.heading.nativeElement.textContent}`);
+  }
+
+  ngAfterViewInit() {
+    console.log(`Local reference heading ngAfterViewInit: ${this.heading.nativeElement.textContent}`);
   }
 
 }
