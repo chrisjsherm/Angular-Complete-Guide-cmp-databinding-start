7c521901d59032d1cb96828aec1eea6166981469
Chris Sherman <chris.sherman@outlook.com>
2019-02-23 11:13:06 -0500

    Demonstrate using local references in templates.

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -16,16 +16,16 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddServer() {
+  onAddServer(serverNameInput: HTMLInputElement) {
     this.serverCreated.emit({
-      serverName: this.newServerName, serverContent: this.newServerContent
+      serverName: serverNameInput.value, serverContent: this.newServerContent
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(serverNameInput: HTMLInputElement) {
     this.blueprintCreated.emit(
       {
-        serverName: this.newServerName, serverContent: this.newServerContent
+        serverName: serverNameInput.value, serverContent: this.newServerContent
       }
     );
   }
